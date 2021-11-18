7cafb19bba1beaf1791d02f25d1d3dd4c4602e43
Work <Work@Michaels-MacBook-Pro.local>
2021-11-18 12:08:23 +0000

    quick commit, finally cleared out all BV stuff from resistance branch

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,4 @@ from .Marinescu2016 import Marinescu2016
 from .Marinescu2018 import Marinescu2018
 from .Hua2019 import Hua2019
 from .Cornish2021 import Cornish2021
-from .Cornish2022 import Cornish2022
-from .Cornish2021_sigmoid import Cornish2021_sigmoid
 
